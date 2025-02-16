b92de09fe7716cdeec8500c9ed949fb6007524ce
Luis Izquierdo <leip1493@gmail.com>
2025-02-16 00:52:08 -0400

    feat: getPlayerMatches and MatchDto

--- app/src/main/java/com/leip1493/evolutionygoapp/core/api/ApiClient.kt
+++ app/src/main/java/com/leip1493/evolutionygoapp/core/api/ApiClient.kt
@@ -1,5 +1,6 @@
 package com.leip1493.evolutionygoapp.core.api
 
+import com.leip1493.evolutionygoapp.core.api.dto.MatchDTO
 import com.leip1493.evolutionygoapp.core.api.dto.PlayerDTO
 import retrofit2.Response
 import retrofit2.http.GET
@@ -24,4 +25,13 @@ interface ApiClient {
         @Query("banListName") banListName: String,
         @Query("season") season: Int,
     ): Response<PlayerDTO>
+
+    @GET("api/v1/users/{userId}/matches")
+    suspend fun getPlayerMatches(
+        @Path("userId") userId: String,
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("season") season: Int,
+        @Query("banListName") banListName: String?,
+    ): Response<List<MatchDTO>>
 }
